ac89affa08a389eeb10ff787250165e2f46812ff
Eric Patrick <epatrick@uci.edu>
2023-05-16 13:10:21 -0700

    Demo Levels 1-2
    Setting Change Scene Scene Index to -1 will load the next scene in build settings, -2 to load the previous.

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -7,9 +7,14 @@ public class ChangeScene : MonoBehaviour
 {
     [SerializeField]
     private int SceneIndex = 0;
+   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (SceneIndex == -1)
+            SceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        else if (SceneIndex == -2)
+            SceneIndex =(SceneManager.GetActiveScene().buildIndex -1);
         StartCoroutine(FindObjectOfType<SceneTransition>().LeaveScene(SceneIndex));
     }
 }
