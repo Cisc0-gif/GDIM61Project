f733184f84659173bfd518874f83b8af20adfd2f
Cisc0-gif <Ecorp7@protonmail.com>
2023-05-18 16:12:03 -0700

    Added red light to closed Exit door sign

--- Assets/LevelDoor.cs
+++ Assets/LevelDoor.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LevelDoor : MonoBehaviour
 {
+
+    [SerializeField]
+    private Light2D lightSource;
+
+    [SerializeField]
+    private bool EntranceDoor;
+
     public bool OpenState = false;
     public bool AlwaysOpen = false;
     private Animator animator;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
         Synchronize();
-        
     }
+
     public void ToggleDoor()
     {
         OpenState = !OpenState;
         Synchronize();
     }
+
     private void Synchronize()
     {
         if (OpenState)
@@ -26,5 +36,20 @@ public class LevelDoor : MonoBehaviour
             animator.Play("Close");
         Collision.SetActive(!OpenState);
     }
+
+    void Update()
+    {
+        if (!EntranceDoor) {
+            if (!OpenState)
+            {
+                lightSource.color = new Color(1f, 0, 0, 1f);
+            }
+            else
+            {
+                lightSource.color = new Color(1f, 1f, 1f, 1f);
+            }
+        }
+    }
+
     public GameObject Collision;
 }
